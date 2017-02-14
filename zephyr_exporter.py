137eb1d7d90695cdef6648d10e05d1aa10a8aafe
Davide Moro <davide.moro@gmail.com>
2017-02-14 09:37:54 +0100

    pep8 and labels

--- zephyr_exporter.py
+++ zephyr_exporter.py
@@ -20,6 +20,7 @@ def parse_xml():
         has_comments = []
         has_attachments = []
         no_steps = []
+        labels = []
 
         for item in ddd.find_all('item'):
             row = {}
@@ -30,9 +31,15 @@ def parse_xml():
             row['link'] = item.link.get_text()
             row['priority'] = item.priority.get_text()
             row['reporter'] = item.reporter.get_text()
-            row['labels'] = [label.get_text() for label in item.find_all('label')]
-            row['links'] = [link.get_text() for link in item.find_all('issuekey') if link.parent.parent.get('description') == 'tests']
-            row['attachments'] = [attachment.get('name') for attachment in item.find_all('attachment') if attachment.get('name')]
+            row['labels'] = [label.get_text() for label in
+                             item.find_all('label')]
+            labels.extend(row['labels'])
+            row['links'] = [link.get_text() for link in
+                            item.find_all('issuekey') if
+                            link.parent.parent.get('description') == 'tests']
+            row['attachments'] = [attachment.get('name') for attachment in
+                                  item.find_all('attachment') if
+                                  attachment.get('name')]
 
             if row['attachments']:
                 has_attachments.append(key)
@@ -51,27 +58,44 @@ def parse_xml():
                 multisteps.append(key)
             elif len_steps == 2:
                 steps = item.find('steps')
-                row['steps'] = _restify(steps.find('step').find('step').get_text())
+                row['steps'] = _restify(
+                    steps.find('step').find('step').get_text()
+                )
                 row['data'] = _restify(steps.find('data').get_text())
                 row['result'] = _restify(steps.find('result').get_text())
 
             if key not in multisteps:
                 results.append(row)
 
-        print "********* results ({0}) ******************".format(len(results))
+        labels = sorted(list(set(labels)), key=str.lower)
+
+        print "********* results ({0}) ******************".format(
+            len(results))
         print pprint(results)
 
-        print "********* has comments ({0}) ******************".format(len(has_comments))
+        print "********* has comments ({0}) ******************".format(
+            len(has_comments))
         print pprint(has_comments)
 
-        print "********* has attachments ({0}) ******************".format(len(has_attachments))
+        print "********* has attachments ({0}) ******************".format(
+            len(has_attachments))
         print pprint(has_attachments)
 
-        print "********* no steps ({0}) ******************".format(len(no_steps))
+        print "********* no steps ({0}) ******************".format(
+            len(no_steps))
         print pprint(no_steps)
 
-        print "********* multisteps ({0}) ******************".format(len(multisteps))
+        print "********* multisteps ({0}) ******************".format(
+            len(multisteps))
         print pprint(multisteps)
+
+        print "********* labels ({0}) ******************".format(
+            len(labels))
+        print pprint(labels)
 
 if __name__ == '__main__':
     parse_xml()
+
+
+# TODO:
+# clean labels
